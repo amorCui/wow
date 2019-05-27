f0bb7cdfea25f797307b39629635c49de6449469
amorCui <43329030+amorCui@users.noreply.github.com>
2019-05-28 01:06:57 +0800

    route test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './Root';
+import { HashRouter,Switch,Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import Tab from './Tab';
+
+
+ReactDOM.render(
+
+<HashRouter basename='/'>
+    <Route path="/" component={Root}/>
+</HashRouter>
+,
+document.getElementById('root')
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
