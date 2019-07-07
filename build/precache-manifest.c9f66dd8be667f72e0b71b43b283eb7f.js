fb9a41633b848949bb1eb31928aa01992be43d97
amorCui <43329030+amorCui@users.noreply.github.com>
2019-07-08 05:04:20 +0800

    build
    build

---
+++ build/precache-manifest.c9f66dd8be667f72e0b71b43b283eb7f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5cc8a4e65ba320bb3f5667a00d84fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "decdb98e5abbf18a3a56",
+    "url": "./static/css/main.1b0f41e8.chunk.css"
+  },
+  {
+    "revision": "0d78a2a63b0ccf28bcfa",
+    "url": "./static/js/2.57e4bda9.chunk.js"
+  },
+  {
+    "revision": "decdb98e5abbf18a3a56",
+    "url": "./static/js/main.953c5100.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "fd71e58262475630fe7dec0d55c60163",
+    "url": "./static/media/74.fd71e582.svg"
+  },
+  {
+    "revision": "7e5136650e1161d440bdf2871c36712e",
+    "url": "./static/media/dz.7e513665.svg"
+  },
+  {
+    "revision": "3993bb0daca14c24261664c8c540d8c2",
+    "url": "./static/media/fs.3993bb0d.svg"
+  },
+  {
+    "revision": "aa6f8f2897633db5b2a04fb95a653618",
+    "url": "./static/media/lr.aa6f8f28.svg"
+  },
+  {
+    "revision": "324fbef448b5c41bfd3bc7582000cf0c",
+    "url": "./static/media/ms.324fbef4.svg"
+  },
+  {
+    "revision": "66f2e81ccfb78ed9fd7661ec05ca73a8",
+    "url": "./static/media/sm.66f2e81c.svg"
+  },
+  {
+    "revision": "9db1dd962f1cb221285cd17420b00829",
+    "url": "./static/media/ss.9db1dd96.svg"
+  },
+  {
+    "revision": "758552f34443ef0cc88260c5b883c023",
+    "url": "./static/media/warrior_0_bg.758552f3.jpg"
+  },
+  {
+    "revision": "f552f527179b39cd66e28d4ca8e85301",
+    "url": "./static/media/warrior_1_bg.f552f527.jpg"
+  },
+  {
+    "revision": "45197581cbcf1c6a6899635e22913835",
+    "url": "./static/media/warrior_2_bg.45197581.jpg"
+  },
+  {
+    "revision": "3d6225e8b99d86b57d8536ccf5934e22",
+    "url": "./static/media/xd.3d6225e8.svg"
+  },
+  {
+    "revision": "1b32b484d0ea7db2af7cf3f98c866063",
+    "url": "./static/media/zs.1b32b484.svg"
+  }
+]);
